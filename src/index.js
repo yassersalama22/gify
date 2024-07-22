9893adcf7bd388bd9c35d03bef3c521d90d33229
Yasser Salama <yassersalama@Yassers-iMac.local>
2024-07-22 14:35:10 +0300

    Add code to search GIPHY's API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-import './style.css';
+import "./style.css";
 
-console.log('Webpack setup complete');
+document
+  .getElementById("search-button")
+  .addEventListener("click", async function () {
+    const searchQuery = document.getElementById("search-box").value;
+    if (searchQuery) {
+      const apiKey = "DU5ohdVnWAiXqnTR1FqOF5pr4ddzJI4b";
+      const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(
+        searchQuery
+      )}&limit=10`;
+
+      try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+          throw new Error("API response is not OK");
+        }
+
+        const data = await response.json();
+        displayGifs(data.data);
+      } catch (error) {
+        console.error("Fetch operation failed:", error);
+      }
+    } else {
+      alert("Please enter search term");
+    }
+  });
+
+function displayGifs(gifs) {
+  gifs.forEach((gif) => {
+    console.log(gif.images.fixed_height.url);
+  });
+}
