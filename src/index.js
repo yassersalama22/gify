8e934b9057fd424461512ac9ab6a04c8eed08092
Yasser Salama <yassersalama@Yassers-iMac.local>
2024-07-22 15:11:25 +0300

    restructure the js code to follow best practices

--- src/index.js
+++ src/index.js
@@ -1,44 +1,49 @@
 import "./style.css";
 
-document
-  .getElementById("search-button")
-  .addEventListener("click", async function () {
-    const searchQuery = document.getElementById("search-box").value;
-    if (searchQuery) {
-      const apiKey = "DU5ohdVnWAiXqnTR1FqOF5pr4ddzJI4b";
-      const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(
-        searchQuery
-      )}&limit=10`;
+// Function to handle search button click
+const handleSearch = async () => {
+  const searchQuery = document.getElementById("search-box").value.trim();
+  if (searchQuery) {
+    const apiKey = "DU5ohdVnWAiXqnTR1FqOF5pr4ddzJI4b";
+    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(
+      searchQuery
+    )}&limit=10`;
 
-      try {
-        const response = await fetch(apiUrl);
+    try {
+      const response = await fetch(apiUrl);
 
-        if (!response.ok) {
-          throw new Error("API response is not OK");
-        }
-
-        const data = await response.json();
-        displayGifs(data.data);
-      } catch (error) {
-        console.error("Fetch operation failed:", error);
+      if (!response.ok) {
+        throw new Error("API response is not OK");
       }
-    } else {
-      alert("Please enter search term");
-    }
-  });
 
-function displayGifs(gifs) {
+      const { data: gifs } = await response.json();
+      renderGifResults(gifs);
+    } catch (error) {
+      console.error("Fetch operation failed:", error);
+    }
+  } else {
+    alert("Please enter a search term");
+  }
+};
+
+// Function to display GIF results
+const renderGifResults = (gifs) => {
   const resultsContainer = document.getElementById("results-container");
-  resultsContainer.innerHTML = "";
+  resultsContainer.innerHTML = ""; // Clear previous results
 
   if (gifs.length > 0) {
-    gifs.forEach((gif) => {
+    gifs.forEach(({ images, title }) => {
       const gifItem = document.createElement("div");
       gifItem.className = "gif-item";
-      gifItem.innerHTML = `<img src="${gif.images.fixed_height.url}" alt="${gif.title}">`;
+      gifItem.innerHTML = `<img src="${images.fixed_height.url}" alt="${title}">`;
       resultsContainer.appendChild(gifItem);
     });
   } else {
     resultsContainer.innerHTML = '<p id="no-results">No results found</p>';
   }
-}
+};
+
+// Event listener for search button
+document
+  .getElementById("search-button")
+  .addEventListener("click", handleSearch);
